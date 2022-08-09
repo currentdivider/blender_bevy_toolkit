8fe5f69c6589ee6bc7ca8e27266f83e49d1f2ff7
James Natulan <66678655+currentdivider@users.noreply.github.com>
2022-08-10 03:43:13 +0800

    Refactored Transform struct

--- blender_bevy_toolkit/definitions/bevy_transform/transform.py
+++ blender_bevy_toolkit/definitions/bevy_transform/transform.py
@@ -9,42 +9,80 @@ from blender_bevy_toolkit.component_base import (
 class Transform(ComponentBase):
     def encode(config, obj):
         """Returns a Component representing this component
-
-        {
+       {
             "type": "bevy_transform::components::transform::Transform",
             "struct": {
                 "translation": {
-                    "type": "glam::vec3::Vec3",
-                    "value": (0.0, 0.0, 0.0),
+                    "type": "glam::f32::vec3::Vec3",
+                    "struct": {
+                        "x": {
+                            "type": "f32",
+                            "value": 0.0,
+                        },
+                        "y": {
+                            "type": "f32",
+                            "value": 0.0,
+                        },
+                        "z": {
+                            "type": "f32",
+                            "value": 0.0,
+                        },
+                    },
                 },
                 "rotation": {
-                    "type": "glam::quat::Quat",
-                    "value": (0.0, 0.0, 0.0, 1.0),
+                    "type": "glam::f32::sse2::quat::Quat",
+                    "value": (0.0,
+                    0.0,
+                    0.0,
+                    1.0),
                 },
                 "scale": {
-                    "type": "glam::vec3::Vec3",
-                    "value": (1.0, 1.0, 1.0),
+                    "type": "glam::f32::vec3::Vec3",
+                    "struct": {
+                        "x": {
+                            "type": "f32",
+                            "value": 1.0,
+                        },
+                        "y": {
+                            "type": "f32",
+                            "value": 1.0,
+                        },
+                        "z": {
+                            "type": "f32",
+                            "value": 1.0,
+                        },
+                    },
                 },
+            },
         }
         """
-        transform = obj.matrix_world
+
         if obj.parent is None:
             transform = obj.matrix_world
         else:
             transform = obj.matrix_local
 
         position, rotation, scale = transform.decompose()
-        bevy_pos = (position[0], position[2], position[1])
-        # position = (0.0, 0.0, 0.0)
-        # rotation = (1.0, 0.0, 0.0, 0.0)
-        # scale = (1.0, 1.0, 1.0)
-
         return rust_types.Map(
             type="bevy_transform::components::transform::Transform",
             struct=rust_types.Map(
-                translation=rust_types.Vec3(bevy_pos),
+                translation=rust_types.Map(
+                    type="glam::f32::vec3::Vec3",
+                    struct=rust_types.Map(
+                        x=rust_types.F32(position.x),
+                        y=rust_types.F32(position.y),
+                        z=rust_types.F32(position.z),
+                    )
+                ),
                 rotation=rust_types.Quat(rotation),
-                scale=rust_types.Vec3(scale),
+                scale=rust_types.Map(
+                    type="glam::f32::vec3::Vec3",
+                    struct=rust_types.Map(
+                        x=rust_types.F32(scale.x),
+                        y=rust_types.F32(scale.y),
+                        z=rust_types.F32(scale.z),
+                    )
+                ),
             ),
         )
 
