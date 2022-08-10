1f3a5d2e5d55bccff94029dda223e9770a1ce6b1
James Natulan <66678655+currentdivider@users.noreply.github.com>
2022-08-11 03:17:47 +0800

    updated global_transform

--- blender_bevy_toolkit/definitions/bevy_transform/global_transform.py
+++ blender_bevy_toolkit/definitions/bevy_transform/global_transform.py
@@ -11,32 +11,147 @@ class GlobalTransform(ComponentBase):
         """Returns a Component representing this component
 
         {
-            "type": "bevy_transform::components::transform::GlobalTransform",
+        "type": "bevy_transform::components::global_transform::GlobalTransform",
+        "tuple_struct": [
+          {
+            "type": "glam::f32::affine3a::Affine3A",
             "struct": {
-                "translation": {
-                    "type": "glam::vec3::Vec3",
-                    "value": (0.0, 0.0, 0.0),
+              "matrix3": {
+                "type": "glam::f32::sse2::mat3a::Mat3A",
+                "struct": {
+                  "x_axis": {
+                    "type": "glam::f32::sse2::vec3a::Vec3A",
+                    "struct": {
+                      "x": {
+                        "type": "f32",
+                        "value": 1.0,
+                      },
+                      "y": {
+                        "type": "f32",
+                        "value": 0.0,
+                      },
+                      "z": {
+                        "type": "f32",
+                        "value": 0.0,
+                      },
+                    },
+                  },
+                  "y_axis": {
+                    "type": "glam::f32::sse2::vec3a::Vec3A",
+                    "struct": {
+                      "x": {
+                        "type": "f32",
+                        "value": 0.0,
+                      },
+                      "y": {
+                        "type": "f32",
+                        "value": 1.0,
+                      },
+                      "z": {
+                        "type": "f32",
+                        "value": 0.0,
+                      },
+                    },
+                  },
+                  "z_axis": {
+                    "type": "glam::f32::sse2::vec3a::Vec3A",
+                    "struct": {
+                      "x": {
+                        "type": "f32",
+                        "value": 0.0,
+                      },
+                      "y": {
+                        "type": "f32",
+                        "value": 0.0,
+                      },
+                      "z": {
+                        "type": "f32",
+                        "value": 1.0,
+                      },
+                    },
+                  },
                 },
-                "rotation": {
-                    "type": "glam::quat::Quat",
-                    "value": (0.0, 0.0, 0.0, 1.0),
+              },
+              "translation": {
+                "type": "glam::f32::sse2::vec3a::Vec3A",
+                "struct": {
+                  "x": {
+                    "type": "f32",
+                    "value": 0.0,
+                  },
+                  "y": {
+                    "type": "f32",
+                    "value": 0.0,
+                  },
+                  "z": {
+                    "type": "f32",
+                    "value": 0.0,
+                  },
                 },
-                "scale": {
-                    "type": "glam::vec3::Vec3",
-                    "value": (1.0, 1.0, 1.0),
-                },
-        }
+              },
+            },
+          },
+        ],
+      },
         """
 
         transform = obj.matrix_world
 
-        position, rotation, scale = transform.decompose()
+        # position, rotation, scale = transform.decompose()
+        x_axis = [transform[0][0], transform[0]
+                  [1], transform[0][2], transform[0][3]]
+        y_axis = [transform[1][0], transform[1]
+                  [1], transform[1][2], transform[1][3]]
+        z_axis = [transform[2][0], transform[2]
+                  [1], transform[2][2], transform[2][3]]
+        w_axis = [transform[3][0], transform[3]
+                  [1], transform[3][2], transform[3][3]]
+
         return rust_types.Map(
             type="bevy_transform::components::global_transform::GlobalTransform",
-            struct=rust_types.Map(
-                translation=rust_types.Vec3(position),
-                rotation=rust_types.Quat(rotation),
-                scale=rust_types.Vec3(scale),
+            tuple_struct=rust_types.List(
+                rust_types.Map(
+                    type="glam::f32::affine3a::Affine3A",
+                    struct=rust_types.Map(
+                        matrix3=rust_types.Map(
+                            type="glam::f32::sse2::mat3a::Mat3A",
+                            struct=rust_types.Map(
+                                x_axis=rust_types.Map(
+                                    type="glam::f32::sse2::vec3a::Vec3A",
+                                    struct=rust_types.Map(
+                                        x=rust_types.F32(x_axis[0]),
+                                        y=rust_types.F32(x_axis[1]),
+                                        z=rust_types.F32(x_axis[2]),
+                                    )
+                                ),
+                                y_axis=rust_types.Map(
+                                    type="glam::f32::sse2::vec3a::Vec3A",
+                                    struct=rust_types.Map(
+                                        x=rust_types.F32(y_axis[0]),
+                                        y=rust_types.F32(y_axis[1]),
+                                        z=rust_types.F32(y_axis[2]),
+                                    )
+                                ),
+                                z_axis=rust_types.Map(
+                                    type="glam::f32::sse2::vec3a::Vec3A",
+                                    struct=rust_types.Map(
+                                        x=rust_types.F32(z_axis[0]),
+                                        y=rust_types.F32(z_axis[1]),
+                                        z=rust_types.F32(z_axis[2]),
+                                    )
+                                )
+                            )
+                        ),
+                        translation=rust_types.Map(
+                            type="glam::f32::sse2::vec3a::Vec3A",
+                            struct=rust_types.Map(
+                                x=rust_types.F32(w_axis[0]),
+                                y=rust_types.F32(w_axis[1]),
+                                z=rust_types.F32(w_axis[2]),
+                            )
+                        )
+                    )
+                ),
             ),
         )
 
