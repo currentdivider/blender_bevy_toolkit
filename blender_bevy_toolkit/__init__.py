86f11ca6c2ce16d93a419b430fa794d9ac726340
James Natulan <66678655+currentdivider@users.noreply.github.com>
2022-08-18 13:12:58 +0800

    Remove transform data from exported gltf files

--- blender_bevy_toolkit/__init__.py
+++ blender_bevy_toolkit/__init__.py
@@ -10,8 +10,14 @@ import logging
 
 import bpy
 from bpy.app.handlers import persistent  # pylint: disable=E0401
+from bpy.props import (BoolProperty,
+                       IntProperty,
+                       FloatProperty,
+                       StringProperty,
+                       EnumProperty,
+                       CollectionProperty,
+                       )
 from bpy_extras.io_utils import ExportHelper
-
 
 from .utils import jdict
 from . import components
@@ -72,7 +78,8 @@ def unregister():
     bpy.app.handlers.load_post.remove(load_handler)
 
     for component in component_base.COMPONENTS:
-        logger.info(jdict(event="unregistering_component", component=str(component)))
+        logger.info(jdict(event="unregistering_component",
+                    component=str(component)))
         component.unregister()
     logger.info(jdict(event="unregistering_bevy_addon", state="end"))
 
@@ -87,7 +94,8 @@ def load_handler(_dummy):
     operators.update_all_component_list()
 
     for component in component_base.COMPONENTS:
-        logger.info(jdict(event="registering_component", component=str(component)))
+        logger.info(jdict(event="registering_component",
+                    component=str(component)))
         component.register()
 
 
@@ -106,7 +114,135 @@ class ExportBevy(bpy.types.Operator, ExportHelper):
     filename_ext = ".scn"
     filter_glob: bpy.props.StringProperty(default="*.scn", options={"HIDDEN"})
 
-    def execute(self, _context):
+    # List of operator properties, the attributes will be assigned
+    # to the class instance from the operator setting before calling.
+    export_gltf: BoolProperty(
+        name="Export GLTFs",
+        description="Export GLTF file for each object",
+        default=True,
+    )
+
+    batch_export_format: EnumProperty(
+        name='Format',
+        items=(('GLB', 'glTF Binary (.glb)',
+                'Exports a single file, with all data packed in binary form. '
+                'Most efficient and portable, but more difficult to edit later'),
+               ('GLTF_EMBEDDED', 'glTF Embedded (.gltf)',
+                'Exports a single file, with all data packed in JSON. '
+                'Less efficient than binary, but easier to edit later'),
+               ('GLTF_SEPARATE', 'glTF Separate (.gltf + .bin + textures)',
+                'Exports multiple files, with separate JSON, binary and texture data. '
+                'Easiest to edit later')),
+        description=(
+            'Output format and embedding options. Binary is most efficient, '
+            'but JSON (embedded or separate) may be easier to edit later'
+        ),
+        default='GLB'
+    )
+
+    batch_export_copyright: StringProperty(
+        name='Copyright',
+        description='Legal rights and conditions for the model',
+        default=''
+    )
+
+    batch_export_image_format: EnumProperty(
+        name='Images',
+        items=(('AUTO', 'Automatic',
+                'Save PNGs as PNGs and JPEGs as JPEGs.\n'
+                'If neither one, use PNG'),
+               ('JPEG', 'JPEG Format (.jpg)',
+                'Save images as JPEGs. (Images that need alpha are saved as PNGs though.)\n'
+                'Be aware of a possible loss in quality'),
+               ),
+        description=(
+            'Output format for images. PNG is lossless and generally preferred, but JPEG might be preferable for web '
+            'applications due to the smaller file size'
+        ),
+        default='AUTO'
+    )
+
+    batch_export_selection: BoolProperty(
+        name="Selection Only",
+        description="Export selected objects only",
+        default=True,
+    )
+
+    batch_export_levels: IntProperty(
+        name='Collection Levels',
+        description='Set the levels of collections',
+        default=2
+    )
+
+    batch_export_materials: EnumProperty(
+        name='Materials',
+        items=(('EXPORT', 'Export',
+                'Export all materials used by included objects'),
+               ('PLACEHOLDER', 'Placeholder',
+                'Do not export materials, but write multiple primitive groups per mesh, keeping material slot information'),
+               ('NONE', 'No export',
+                'Do not export materials, and combine mesh primitive groups, losing material slot information')),
+        description='Export materials ',
+        default='EXPORT'
+    )
+
+    batch_export_cameras: BoolProperty(
+        name='Export Cameras',
+        description='Export cameras',
+        default=False
+    )
+
+    batch_export_extras: BoolProperty(
+        name='Export Custom Properties',
+        description='Export custom properties as glTF extras',
+        default=False
+    )
+
+    batch_export_apply: BoolProperty(
+        name='Export Apply Modifiers',
+        description='Apply modifiers (excluding Armatures) to mesh objects -'
+                    'WARNING: prevents exporting shape keys',
+        default=False
+    )
+
+    batch_export_yup: BoolProperty(
+        name='+Y Up',
+        description='Export using glTF convention, +Y up',
+        default=True
+    )
+
+    batch_export_texcoords: BoolProperty(
+        name='UVs',
+        description='Export UVs (texture coordinates) with meshes',
+        default=True
+    )
+
+    batch_export_normals: BoolProperty(
+        name='Normals',
+        description='Export vertex normals with meshes',
+        default=True
+    )
+
+    batch_export_tangents: BoolProperty(
+        name='Tangents',
+        description='Export vertex tangents with meshes',
+        default=False
+    )
+
+    batch_export_colors: BoolProperty(
+        name='Vertex Colors',
+        description='Export vertex colors with meshes',
+        default=True
+    )
+
+    batch_export_apply: BoolProperty(
+        name='Apply Modifiers',
+        description='Apply modifiers (excluding Armatures) to mesh objects -'
+                    'WARNING: prevents exporting shape keys',
+        default=False
+    )
+
+    def execute(self, context):
         """Begin the export"""
 
         if not self.filepath:
@@ -121,6 +257,50 @@ class ExportBevy(bpy.types.Operator, ExportHelper):
                 "make_duplicates_real": False,
             }
         )
+
+        # Blender GLTF Exporter
+        if self.export_gltf:
+            # get the folder
+            folder_path = os.path.dirname(self.filepath)
+
+            # get objects selected in the viewport
+            viewport_selection = context.selected_objects
+            obj_export_list = [i for i in context.scene.objects]
+
+            # deselect all objects
+            bpy.ops.object.select_all(action='DESELECT')
+
+            for item in obj_export_list:
+                item.select_set(True)
+                # if item.type == 'MESH':
+                file_path = os.path.join(folder_path, f"{item.name}")
+
+                item.location = [0.0, 0.0, 0.0]                
+                item.rotation_quaternion = [1.0, 0.0, 0.0, 0.0]
+                item.scale = [1.0, 1.0, 1.0]
+
+                bpy.ops.export_scene.gltf(
+                    filepath=file_path,
+                    use_select=self.batch_export_selection,
+                    export_format=self.batch_export_format,
+                    export_copyright=self.batch_export_copyright,
+                    export_image_format=self.batch_export_image_format,
+                    export_materials=self.batch_export_materials,
+                    export_colors=self.batch_export_colors,
+                    export_cameras=self.batch_export_cameras,
+                    export_extras=self.batch_export_extras,
+                    export_yup=self.batch_export_yup,
+                    export_apply=self.batch_export_apply,
+                    export_texcoords=self.batch_export_texcoords,
+                    export_normals=self.batch_export_normals,
+                    export_tangents=self.batch_export_tangents,
+                )
+
+                item.select_set(False)
+
+            # restore viewport selection
+            for ob in viewport_selection:
+                ob.select_set(True)
 
         return {"FINISHED"}
 
